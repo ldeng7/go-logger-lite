648c3c56b774c2bc89fb3fa117bb2776df14e887
Lynn Deng <beednln@163.com>
2022-06-19 12:54:19 +0800

    generics!

--- logger/logx.go
+++ logger/logx.go
@@ -85,7 +85,7 @@ func (l *Logger) FileRotate() (*os.File, error) {
 	return file, nil
 }
 
-func (l *Logger) log(calldepth int, level int, v []interface{}) {
+func (l *Logger) log(calldepth int, level int, v []any) {
 	if level < l.LogLevel {
 		return
 	}
@@ -101,7 +101,7 @@ func (l *Logger) log(calldepth int, level int, v []interface{}) {
 	}
 }
 
-func (l *Logger) logf(calldepth int, level int, format string, v []interface{}) {
+func (l *Logger) logf(calldepth int, level int, format string, v []any) {
 	if level < l.LogLevel {
 		return
 	}
@@ -117,58 +117,58 @@ func (l *Logger) logf(calldepth int, level int, format string, v []interface{}) 
 	}
 }
 
-func (l *Logger) Log(level int, v ...interface{}) {
+func (l *Logger) Log(level int, v ...any) {
 	l.log(3, level, v)
 }
 
-func (l *Logger) Logf(level int, format string, v ...interface{}) {
+func (l *Logger) Logf(level int, format string, v ...any) {
 	l.logf(3, level, format, v)
 }
 
-func (l *Logger) Info(v ...interface{}) {
+func (l *Logger) Info(v ...any) {
 	l.log(3, INFO, v)
 }
 
-func (l *Logger) Infof(format string, v ...interface{}) {
+func (l *Logger) Infof(format string, v ...any) {
 	l.logf(3, INFO, format, v)
 }
 
-func (l *Logger) Notice(v ...interface{}) {
+func (l *Logger) Notice(v ...any) {
 	l.log(3, NOTICE, v)
 }
 
-func (l *Logger) Noticef(format string, v ...interface{}) {
+func (l *Logger) Noticef(format string, v ...any) {
 	l.logf(3, NOTICE, format, v)
 }
 
-func (l *Logger) Warn(v ...interface{}) {
+func (l *Logger) Warn(v ...any) {
 	l.log(3, WARN, v)
 }
 
-func (l *Logger) Warnf(format string, v ...interface{}) {
+func (l *Logger) Warnf(format string, v ...any) {
 	l.logf(3, WARN, format, v)
 }
 
-func (l *Logger) Err(v ...interface{}) {
+func (l *Logger) Err(v ...any) {
 	l.log(3, ERR, v)
 }
 
-func (l *Logger) Errf(format string, v ...interface{}) {
+func (l *Logger) Errf(format string, v ...any) {
 	l.logf(3, ERR, format, v)
 }
 
-func (l *Logger) Crit(v ...interface{}) {
+func (l *Logger) Crit(v ...any) {
 	l.log(3, CRIT, v)
 }
 
-func (l *Logger) Critf(format string, v ...interface{}) {
+func (l *Logger) Critf(format string, v ...any) {
 	l.logf(3, CRIT, format, v)
 }
 
-func (l *Logger) Emerg(v ...interface{}) {
+func (l *Logger) Emerg(v ...any) {
 	l.log(3, EMERG, v)
 }
 
-func (l *Logger) Emergf(format string, v ...interface{}) {
+func (l *Logger) Emergf(format string, v ...any) {
 	l.logf(3, EMERG, format, v)
 }
